5563b6c3cbabf205d2aa1479500a6af056c48cb3
Joshie Fishbein <joshie@sanctuary.computer>
2019-08-16 18:53:16 -0400

    init commander and create placeholder function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('ding');
+#!/usr/bin/env node
+
+const candelabra = require('commander');
+const { description, version } = require('./package.json');
+
+candelabra.version(version).description(description);
+
+candelabra.command('* <url>').action(url => {
+  console.log(url);
+});
+
+candelabra.parse(process.argv);
