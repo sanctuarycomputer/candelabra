e88c8d45c1a7139176b99cd7c9df0116d4fae4d5
Joshie Fishbein <joshie@sanctuary.computer>
2019-08-16 20:04:09 -0400

    use puppeteer to get all internal links on current requested page

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
 const candelabra = require('commander');
 const { description, version } = require('./package.json');
 
+const generateSitemap = require('./lib/generateSitemap');
+
 candelabra.version(version).description(description);
 
 candelabra.command('* <url>').action(url => {
-  console.log(url);
+  console.log('Building a site map for: ', url);
+
+  const sitemap = generateSitemap(url);
 });
 
 candelabra.parse(process.argv);
