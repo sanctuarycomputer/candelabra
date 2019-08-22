05aa3db415dd938200762a7ec24248fa7f07f550
Joshie Fishbein <joshie@sanctuary.computer>
2019-08-22 18:36:30 -0400

    paginate properly, compile violations, add outputs

--- index.js
+++ index.js
@@ -3,14 +3,12 @@
 const candelabra = require('commander');
 const { description, version } = require('./package.json');
 
-const generateSitemap = require('./lib/generateSitemap');
+const checkWebsiteForViolations = require('./lib/checkWebsiteForViolations');
 
 candelabra.version(version).description(description);
 
 candelabra.command('* <url>').action(url => {
-  console.log('Building a site map for: ', url);
-
-  const sitemap = generateSitemap(url);
+  const sitemap = checkWebsiteForViolations(url);
 });
 
 candelabra.parse(process.argv);
